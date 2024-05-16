e26f64a3090d48b309ba2d0cf06d83365079ff6b
victoriapelagio <victoriapelagio2@gmail.com>
2024-05-16 09:31:38 -0700

    lab 9
    lab 9

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,24 +1,16 @@
+//index.js - to utilize jQuery to make a special button
 //Author: Victoria Pelagio <vpelagio@ucsc.edu>
 // Date: April 25, 2024
 // ART 101 Lab 5
 
-// Define Variables
-let make = "Corolla";
-let model = "le";
-let color = "White";
-let year = 2009; //Change to a number
-let ownIt = true;
-let currentYear = 2024;
+$(#My-button").append("<button id='my-button'>name button</button>");
 
-//calculate
-let age = currentYear - year;
 
-//output
-document.writenln("Make: " + make + "<br>");
-document.writenln("Make: " + model + "<br>");
-document.writenln("Make: " + color+ "<br>");
-document.writenln("Make: " + year + "<br>");
-document.writenln("Make: " + age + "<br>");
+$(document).on("click", '#My-button', function(){
+    var name = prompt("name please: ")
+    $('#title').html("Hello"+name)
+});
+
 
 
 
